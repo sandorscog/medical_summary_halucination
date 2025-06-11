490957a780948c919666b2f19ec96ce55cb12eda
Scoggin <sandorscog@gmail.com>
2025-06-11 02:11:32 -0300

    Adds a draft of the main pipeline and fixes-adapts several other things

---
+++ main.py
@@ -0,0 +1,26 @@
+from modules.summarizer import Summarizer
+from modules.SummaryManager import EvalSummaryManager
+from modules.summary_enhancer import enhance
+
+
+
+def main():
+
+    data = load_data()
+    
+    summarizer = Summarizer(model='bart')
+    evaluator = EvalSummaryManager()
+    for case in data:
+
+        original_text = case['discharge']
+        base_summary = summarizer.summ(original_text)
+        base_evaluation = evaluator.eval_summary(summary=base_summary, original=original_text)
+
+        enhanced_summary = enhance(base_summary, case)
+        enhanced_evaluation = evaluator.eval_summary(summary=base_summary, original=original_text)
+
+
+
+
+if __name__ == '__main__':
+    main()
