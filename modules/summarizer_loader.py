5e8603e8cd536c33e26c3b3c1552065d8bf936c6
Scoggin <sandorscog@gmail.com>
2025-07-10 04:55:33 -0300

    Adds gemini to the eval pipeline and summarizer class

--- modules/summarizer_loader.py
+++ modules/summarizer_loader.py
@@ -1,4 +1,6 @@
 from transformers import BartTokenizer, BartForConditionalGeneration, AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+import google.generativeai as genai
+import json
 
 def load_bart():
     tokenizer = BartTokenizer.from_pretrained("facebook/bart-large-cnn")
@@ -19,3 +21,13 @@ def load_clinical_t5_pipeline():
     tokenizer = AutoTokenizer.from_pretrained("hossboll/clinical-t5")
     model = AutoModelForSeq2SeqLM.from_pretrained("hossboll/clinical-t5")
     return pipeline("summarization", model=model, tokenizer=tokenizer)
+
+def load_gemini(model: str='gemini-2.5-flash'):
+
+    with open('config/cred.json') as f:
+        config = json.load(f)
+    api_key = config['api_key']
+
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel(model_name=model)
+    return model
