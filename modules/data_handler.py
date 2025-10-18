c0d75d02a231222a1204575af8bd60c38b0e8d94
Scoggin <sandorscog@gmail.com>
2025-10-18 01:15:09 -0300

    Adapts the main pipeline to create the time frames for each discharge

--- modules/data_handler.py
+++ modules/data_handler.py
@@ -1,12 +1,14 @@
+import os
+
 import pandas as pd
 
 def load_discharges():
-    data = pd.read_csv('discharges.csv') ### TODO: Define the file name/path
-    return data
+    data = pd.read_csv('data_samples/notes/discharge.csv')
+    return data[:3]
 
 def load_patient_data():
-    data = pd.read_csv('patients.csv') ### TODO: Define the file name/path
-    return data
+    data = pd.read_csv('data_samples/hosp/pharmacy.csv') ### TODO: Remove limit in the return
+    return data[:100] 
 
 def load_data():
     discharges = load_discharges()
